fa182219f79cf446968cf32a16f87bdff9861a92
Farizaaaa <farizaaaa10@gmail.com>
2023-11-25 22:22:01 +0530

    Firebase Files Upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/firebase_options.dart';
 import 'package:flutter_spotify_ui/views/login/Spotify_Login_Page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
